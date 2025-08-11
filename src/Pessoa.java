538b997f777ed6f3b37005b401d6ff69274c42f1
CaioXys <caiofer0515@gmail.com>
2025-08-11 16:58:48 -0300

    Separando as classes; atribuindo impressões; protected nos atributos em Pessoa

--- src/Pessoa.java
+++ src/Pessoa.java
@@ -1,36 +1,11 @@
 public abstract class Pessoa {
 
-    private String nome;
-    private int idade;
-    private String sexo;
+    protected String nome;
+    protected int idade;
+    protected String sexo;
 
     public final void fazerAniv() {
         this.idade++;
-    }
-
-    class Visitante extends Pessoa {
-
-    }
-
-    class Aluno extends Pessoa {
-        private int matricula;
-        private String curso;
-        public void PagarMensalidade() {
-
-        }
-    }
-
-    class Bolsista extends Aluno {
-        private int bolsa;
-        public void RenovarBolsa() {
-
-        }
-
-        @Override
-        public void PagarMensalidade() {
-
-        }
-
     }
 
     public String getNome() {
@@ -57,9 +32,12 @@ public abstract class Pessoa {
         this.sexo = sexo;
     }
 
-    // Pessoa p1 = new Pessoa(); // Não funciona pois a classe é abstrata
-    Visitante v1 = new Visitante();
-    Aluno a1 = new Aluno();
-    Bolsista b1 = new Bolsista();
-
+    @Override
+    public String toString() {
+        return "Dados{" +
+                "nome='" + nome + '\'' +
+                ", idade=" + idade +
+                ", sexo='" + sexo + '\'' +
+                '}';
+    }
 }
